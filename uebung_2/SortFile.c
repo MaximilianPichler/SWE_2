12389f371a7f079f59a0a9d8d14c13e7f81ef8f9
Maximilian Pichler <maximilian.pichler97@gmail.com>
2018-03-10 15:01:45 +0100

    Fix SortFile

--- uebung_2/SortFile.c
+++ uebung_2/SortFile.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+void bubblesort(int *array, int length)
+{
+   int i, j,tmp;
+
+   for (i = 0; i < length ; i++) 
+   {
+      for (j = i; j < length ; j++) 
+      {
+          if (array[i] > array[j]) 
+          {
+              tmp = array[i];
+              array[i] = array[j];
+              array[j] = tmp;
+          }
+      }
+   }
+}
+
 int main () {
     srand(time(NULL));
     int buffer[50];
@@ -14,17 +32,18 @@ int main () {
     }
 
     int count = 0;
-
     while (fscanf(fp, "%d", &buffer[count]) == 1) {
         count++;
     }
+    fclose(fp);
 
-    printf("%i\n", count);
+    bubblesort(&buffer[0], count);
+
+    fp = fopen("zahlen.txt", "w+");
 
     for (int i = 0; i < count; i++) {
-        printf("%i\n", buffer[i]);
+        fprintf(fp, "%i\n", buffer[i]);
     }
-
 
     fclose(fp);
     return 0;
